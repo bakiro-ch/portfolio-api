d4ee50a5b9116a591e3d3716c7e69d9a1af522c7
bakiroch <bakirochaif@gmail.com>
2026-07-16 16:36:01 -0700

    fix: environment variables issue

--- src/cloudinary/cloudinary.provider.ts
+++ src/cloudinary/cloudinary.provider.ts
@@ -4,12 +4,6 @@ export const CloudinaryProvider = {
   provide: 'CLOUDINARY',
   
   useFactory: () => {
-        console.log('--- Cloudinary Env Check ---');
-    console.log('Cloud Name:', process.env.CLOUDINARY_CLOUD_NAME);
-    console.log('API Key:', process.env.CLOUDINARY_API_KEY);
-    console.log('API Secret:', process.env.CLOUDINARY_API_SECRET);
-    console.log('------------------------------');
-
     return cloudinary.config({
         cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
